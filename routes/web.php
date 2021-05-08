a752fc9f50c2ea0def30aca82704c86571b05a88
Alberto Luebbert M <alberto@ideashappy.com>
2021-05-08 00:48:06 -0500

    Add new view happybirthdat

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/happy', function() {
+    return 'Feliz cumpleaños GNU Linux Latino!';
+});
